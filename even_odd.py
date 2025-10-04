c9ce454adc891e0b5d45666e6a252b991ac64837
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-10-04 10:50:40 +0530

    added prime

--- even_odd.py
+++ even_odd.py
@@ -1,14 +1,28 @@
-numbers=list(map(int, input("Enter numbers separated by space: ").split()))
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
 
-even_count=0
-odd_count=0
+even_count = 0
+odd_count = 0
+prime_count = 0
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 for num in numbers:
-    if num%2==0:
-        even_count+=1
+    if num % 2 == 0:
+        even_count += 1
     else:
-        odd_count+=1
+        odd_count += 1
 
-print("Total numbers:",len(numbers))
-print("Even numbers:",even_count)
-print("Odd numbers:",odd_count)
+    if is_prime(num):
+        prime_count += 1
+
+print("Total numbers:", len(numbers))
+print("Even numbers:", even_count)
+print("Odd numbers:", odd_count)
+print("Prime numbers:", prime_count)
